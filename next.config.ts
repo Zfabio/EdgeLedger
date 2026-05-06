daadb10e65123619f3fe0fb8654237028fd1c151
Fabio Z <fabiozeqiri@gmail.com>
2026-05-06 19:09:48 +0200

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   // Export as a fully static site for GitHub Pages
   output: "export",
   images: { unoptimized: true },
+  basePath: "/EdgeLedger",
+  assetPrefix: "/EdgeLedger",
 };
 
 export default nextConfig;
